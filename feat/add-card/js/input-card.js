e825bff6f4b613d95308991476240b9b79a8acc2
Madhav B <madhav.b@experionglobal.local>
2023-12-06 17:49:46 +0530

    feat: Add Item to Object Array

--- feat/add-card/js/input-card.js
+++ feat/add-card/js/input-card.js
@@ -7,18 +7,8 @@ buttonClickOff = () => {
 
 addItem = () => {
   var form = document.getElementById("form");
-
   var formData = new FormData(form);
-
-  console.log(formData.get("title"));
-  //   console.log(formData.discription);
-  for (var value of formData.values()) {
-    console.log(value);
-  }
-  console.log(formData.values.value[0]);
-  console.log(typeof formData);
-
-  console.log(Object.keys(formData));
+  addObject(formData.get("title"), formData.get("description"));
 };
 
 $("input[id='title']").on("keyup", function () {
---
+++ feat/store-item/store.js
@@ -0,0 +1,28 @@
+// Initialize the array by retrieving it from local storage, if available
+let todoArray = JSON.parse(localStorage.getItem("todoArray")) || [];
+
+// Function to add a new object to the array and update local storage
+function addObject(title, description) {
+  let newObject = {
+    title: title,
+    description: description,
+  };
+
+  todoArray.push(newObject);
+  updateLocalStorage();
+}
+
+// Function to delete an object from the array based on the title and update local storage
+function deleteObject(title) {
+  todoArray = todoArray.filter((obj) => obj.title !== title);
+  updateLocalStorage();
+}
+
+// Function to update the local storage with the current array
+function updateLocalStorage() {
+  localStorage.setItem("todoArray", JSON.stringify(todoArray));
+}
+
+function displayObject() {
+  console.log(todoArray);
+}
