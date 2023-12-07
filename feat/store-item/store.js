5af4764cb5c08913464c92b21384e37290f1c811
Fazil S <fazil.s@experionglobal.local>
2023-12-07 11:44:08 +0530

    complete backend for delete

--- feat/store-item/store.js
+++ feat/store-item/store.js
@@ -14,8 +14,12 @@ function addObject(title, description) {
 
 // Function to delete an object from the array based on the title and update local storage
 function deleteObject(title) {
-  todoArray = todoArray.filter((obj) => obj.title !== title);
-  updateLocalStorage();
+  let choice = prompt("do you realy want to continue? y/n");
+  if (choice == "y") {
+    todoArray = todoArray.filter((obj) => obj.title !== title);
+    updateLocalStorage();
+    location.reload();
+  }
 }
 
 // Function to update the local storage with the current array
