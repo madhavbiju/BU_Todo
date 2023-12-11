71f41ade50616d889db2ad9965fd10290eaf59e6
Fazil S <fazil.s@experionglobal.local>
2023-12-11 15:03:50 +0530

    added modal bg blur

--- feat/add-card/js/input-card.js
+++ feat/add-card/js/input-card.js
@@ -1,11 +1,16 @@
+// add button on click
 buttonClickOn = () => {
   document.getElementById("card-wrapper").style.display = "block";
   document.getElementById("chat-open-button").style.display = "none";
 };
+
+// add button off click
 buttonClickOff = () => {
   document.getElementById("card-wrapper").style.display = "none";
   document.getElementById("chat-open-button").style.display = "block";
 };
+
+//adding list in todo
 
 addItem = () => {
   var form = document.getElementById("form");
@@ -13,8 +18,26 @@ addItem = () => {
   addObject(formData.get("title"), formData.get("description"));
 };
 
+// title validation  active only when title is inputed
 $("input[id='title']").on("keyup", function () {
   if ($(this).val() != "") {
     $("button[id='add']").removeAttr("disabled");
   }
 });
+
+//background blur when modal open
+
+backgroundBlurOn = () => {
+  document.querySelector(".searchbox").style.filter = "blur(8px)";
+  // document.querySelectorAll(".card").style.filter = "blur(8px)";
+
+  var els = document.querySelectorAll(".card");
+  for (var x = 0; x < els.length; x++) els[x].style.filter = "blur(8px)";
+};
+backgroundBlurOff = () => {
+  document.querySelector(".searchbox").style.filter = "none";
+  // document.querySelectorAll(".card").style.filter = "blur(8px)";
+
+  var els = document.querySelectorAll(".card");
+  for (var x = 0; x < els.length; x++) els[x].style.filter = "none";
+};
