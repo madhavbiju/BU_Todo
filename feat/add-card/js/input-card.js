17cdeee51c97d0b3a1b1b978779565b6e5a808ab
Fazil S <fazil.s@experionglobal.local>
2023-12-11 15:50:57 +0530

    ui fix add card modal

--- feat/add-card/js/input-card.js
+++ feat/add-card/js/input-card.js
@@ -27,7 +27,7 @@ $("input[id='title']").on("keyup", function () {
 
 //background blur when modal open
 
-backgroundBlurOn = 7=> {
+backgroundBlurOn = ()=> {
   document.querySelector(".searchbox").style.filter = "blur(8px)";
   // document.querySelectorAll(".card").style.filter = "blur(8px)";
 
