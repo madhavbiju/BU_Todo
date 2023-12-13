f998ee3c673610380401b6e4a9f58d63c05adff2
Fazil S <fazil.s@experionglobal.local>
2023-12-13 17:28:06 +0530

    edit modal completed

---
+++ feat/edit/js/edit.js
@@ -0,0 +1,56 @@
+// add button on click
+editModalOn = () => {
+  document.getElementById("edit-wrapper").style.display = "block";
+  // document.getElementById("actionButton").style.display = "none";
+  let checkbox = document.querySelectorAll('input[id="checkbox"]:checked');
+
+  console.log(checkbox[0].nextSibling.textContent);
+
+  document
+    .getElementById("editTitle")
+    .setAttribute("value", checkbox[0].nextSibling.textContent);
+  nextSibling = checkbox[0].nextSibling;
+  console.log(nextSibling);
+  console.log(nextSibling.nextSibling.textContent);
+  document.getElementById("edit-description").textContent =
+    nextSibling.nextSibling.textContent;
+  // .setAttribute("value", nextSibling.nextSibling.textContent);
+};
+
+// add button off click
+editModalOff = () => {
+  document.getElementById("edit-wrapper").style.display = "none";
+  // document.getElementById("actionButton").style.display = "block";
+};
+
+//adding list in todo
+
+// addItem = () => {
+//   var form = document.getElementById("form");
+//   var formData = new FormData(form);
+//   addObject(formData.get("title"), formData.get("description"));
+// };
+
+// title validation  active only when title is inputed
+// $("input[id='title']").on("keyup", function () {
+//   if ($(this).val() != "") {
+//     $("button[id='add']").removeAttr("disabled");
+//   }
+// });
+
+//background blur when modal open
+
+// backgroundBlurOn = () => {
+//   document.querySelector(".searchbox").style.filter = "blur(8px)";
+//   // document.querySelectorAll(".card").style.filter = "blur(8px)";
+
+//   var els = document.querySelectorAll(".card");
+//   for (var x = 0; x < els.length; x++) els[x].style.filter = "blur(8px)";
+// };
+// backgroundBlurOff = () => {
+//   document.querySelector(".searchbox").style.filter = "none";
+//   // document.querySelectorAll(".card").style.filter = "blur(8px)";
+
+//   var els = document.querySelectorAll(".card");
+//   for (var x = 0; x < els.length; x++) els[x].style.filter = "none";
+// };
